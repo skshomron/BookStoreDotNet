d8669066e431d8ce2871c2f528e64c47b0035e1d
Shomron <sks.phenix@hotmail.com>
2020-12-30 00:11:56 -0500

    Common project that handle every component used by others projects

---
+++ Common/enums/BookStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.enums
+{
+    public enum BookStatus
+    {
+        Registered,
+        Published,
+        Sold,
+        Deleted,
+
+    }
+}
